cc8997cd5c3d457006a58860aaadf8a4d3988f09
Nicolas Elias <nicolaselias@pm.me>
2022-09-06 22:15:05 -0300

    modulo 1 done

--- src/main/java/com/udemy/compras/QueryGraphQL.java
+++ src/main/java/com/udemy/compras/QueryGraphQL.java
@@ -1,5 +1,8 @@
 package com.udemy.compras;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
@@ -13,5 +16,17 @@ public class QueryGraphQL implements GraphQLQueryResolver {
 
 	public int soma(int a, int b) {
 		return a + b;
+	}
+
+	public Cliente cliente() {
+		return new Cliente("Nicolas", "nicolaselias@pm.me");
+	}
+
+	public List<Cliente> clientes() {
+		List<Cliente> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			list.add(new Cliente("Cliente" + i, "emaildocliente"+i+"@teste.com"));
+		}
+		return list;
 	}
 }
